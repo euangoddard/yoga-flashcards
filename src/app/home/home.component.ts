cb105b6b796c1ab91c7dc40e471062c22de59bbd
Euan Goddard <euan.goddard@gmail.com>
2018-09-22 12:50:18 +0100

    Added pose of the day on to the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
+import { Observable } from 'rxjs';
+import { PosesService, Pose, Poses } from './../poses.service';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'yf-home',
   templateUrl: './home.component.html',
   styles: [],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent {
+  readonly poseOfDay$: Observable<Pose>;
 
-  ngOnInit() {}
+  constructor(private posesService: PosesService) {
+    this.poseOfDay$ = this.posesService.all().pipe(map(poses => this.findPoseOfTheDay(poses)));
+  }
+
+  private findPoseOfTheDay(poses: Poses): Pose {
+    const dateInt = Math.floor(Date.now() / 1000 / 3600);
+    const poseIndex = dateInt % poses.length;
+    return poses[poseIndex];
+  }
 }
