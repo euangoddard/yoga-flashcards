b5c92b9787ee77de127751dcf9145bba82603200
Euan Goddard <euan@stockopedia.com>
2018-09-26 13:47:16 +0100

    Added service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { ROUTES } from './app.routes';
 import { BrowseComponent } from './browse/browse.component';
+import { GlossaryComponent } from './glossary/glossary.component';
 import { HomeComponent } from './home/home.component';
+import { PoseRedirectComponent } from './pose-redirect/pose-redirect.component';
 import { PoseComponent } from './pose/pose.component';
 import { PosesComponent } from './poses/poses.component';
-import { SearchComponent } from './search/search.component';
 import { SearchResultComponent } from './search/search-result/search-result.component';
-import { PoseRedirectComponent } from './pose-redirect/pose-redirect.component';
-import { GlossaryComponent } from './glossary/glossary.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,13 @@ import { GlossaryComponent } from './glossary/glossary.component';
     PoseRedirectComponent,
     GlossaryComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, RouterModule.forRoot(ROUTES)],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(ROUTES),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
