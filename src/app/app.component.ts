0dc9a20553003169550d5cc317376393d8d3d2d7
Euan Goddard <euan@stockopedia.com>
2018-09-30 14:02:41 +0100

    Added option to reload with service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { Observable, of } from 'rxjs';
+import { mapTo, startWith } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'yoga-flashcards',
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  readonly isNewVersionAvailable$: Observable<boolean>;
+
+  constructor(private updates: SwUpdate) {
+    if (environment.production) {
+      this.isNewVersionAvailable$ = this.updates.available.pipe(
+        mapTo(true),
+        startWith(false),
+      );
+    } else {
+      this.isNewVersionAvailable$ = of(false);
+    }
+  }
+
+  ngOnInit(): void {
+    if (environment.production) {
+      this.updates.checkForUpdate();
+    }
+  }
+
+  reload(): void {
+    document.location.reload();
+  }
+}
